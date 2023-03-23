6aa69c3763cd0be02aa1f9be6a752887f23ef0ac
shaheen-uddin <shaheenelt@gmail.com>
2023-03-23 15:40:30 +0600

    Initial Commit

---
+++ build/js/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let stringArr = ['one', 'hey', 'Dave'];
+let guiters = ['strat', 'Les Paul', 5120];
+let mixedata = ['EVH', 1945, true];
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+let stringArr = ['one', 'hey', 'Dave']
+let guiters = ['strat', 'Les Paul', 5120]
+let mixedata = ['EVH', 1945, true]
